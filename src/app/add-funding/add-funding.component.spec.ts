e606d56fa545b501dd249a66e9c4bd01726d0941
Masen Matthews and Roger Yee <roger@>
2018-04-05 09:22:39 -0700

    add initial file structure and routing

---
+++ src/app/add-funding/add-funding.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddFundingComponent } from './add-funding.component';
+
+describe('AddFundingComponent', () => {
+  let component: AddFundingComponent;
+  let fixture: ComponentFixture<AddFundingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddFundingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddFundingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/add-funding/add-funding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-funding',
+  templateUrl: './add-funding.component.html',
+  styleUrls: ['./add-funding.component.css']
+})
+export class AddFundingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
